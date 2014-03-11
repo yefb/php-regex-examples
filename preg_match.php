9f65e772c4dd36ef782b276e8de1c64160f1dbc5
Yeiner.F <yeinerfblog@gmail.com>
2014-03-10 21:19:17 -0500

    preg_match and preg_match_all examples

---
+++ preg_match.php
@@ -0,0 +1,10 @@
+<?php
+
+$text = 'Lorem ipsum dolor dolor sit amet,consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi nisi ut aliquip ex ex ea commodo consequat. Duis aute irure dolor in in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia officia deserunt mollit anim id est laborum.';
+
+// $regex = '#\b(\w+)\s\1\b#';
+
+// preg_match($regex, $text, $matches);
+// preg_match_all($regex, $text, $matches);
+
+// print_r($matches);
